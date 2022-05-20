f9d288384b348002bd7da3f2e5b5adf21caca599
GeorgiaGdaRosa <100375813+GeorgiaGdaRosa@users.noreply.github.com>
2022-05-20 15:49:38 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball
+var groundObj
+var leftSide
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	var ball_options={
+		isStatic: false,
+		restitution: 0.3,
+		friction:0,
+		density:1.2
+	}
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ball = Matter.Bodies.circle(200,10,20,ball_options);
+  	World.add(world,ball);
+
+	  groundObj= new Ground(width/2,670,width,20);
+	  leftSide= new Ground(1100,600,20,120)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  groundObj.show();
+  leftSide.show()
+  
+  drawSprites();
+ 
+}
+
+
+
